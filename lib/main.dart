4aa7e304c0b2d7a0bdc0ea438280dd00daac13ce
BoeDelacruz01 <Boedelacruz@yahoo.com>
2024-07-28 17:24:02 +0800

    removed lint error

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:unilab_order_01/screens/cutomer_list_page.dart';
